7e8445548c80ea5682335aee50cb0086fd27f192
yinqin <yinqin@dxchain.onaliyun.com>
2020-11-02 11:13:04 +0800

    refactor: plop 文件

--- bin/cli.js
+++ bin/cli.js
@@ -4,7 +4,7 @@ const { Plop, run } = require("plop");
 const argv = require("minimist")(args);
 const path = require("path");
 
-const DXCONFIG_PATH = path.join(__dirname, "../src/index.js");
+const PLOP_CONFIG_PATH = path.join(__dirname, "../src/plop.js");
 const command = process.argv.slice(2, 3)[0];
 
 const GENERATOR_COMMAND = ['component', 'page']
@@ -13,7 +13,7 @@ if (GENERATOR_COMMAND.includes(command)) {
   Plop.launch(
     {
       cwd: argv.cwd,
-      configPath: DXCONFIG_PATH,
+      configPath: PLOP_CONFIG_PATH,
       require: argv.require,
       completion: argv.completion,
     },
